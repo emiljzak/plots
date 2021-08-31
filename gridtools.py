1950944906ea7bcc3fe385f881cdfd9e5b2d5461
Emil Zak <emil.zak@desy.de>
2021-08-31 17:19:32 +0200

    grid tools

--- gridtools.py
+++ gridtools.py
@@ -45,9 +45,13 @@ def gen_cartgrid_3D(xrange,yrange,zrange,nptsx,nptsy,nptsz):
     """
     x1d = np.linspace(xrange[0], xrange[1], nptsx, endpoint=True, dtype=float)
     y1d = np.linspace(yrange[0], yrange[1], nptsy, endpoint=True, dtype=float)
-    z1d = np.linspace(zrange[0], zrange[1], nptsz, endpoint=True, dtype=float)
+    z1d = np.linspace(zrange[0], zrange[1], nptsz, endpoint=True, dtype=float) #creates a sequence of numbers (nptsz,)
 
-    xyz  = np.array(list(itertools.product(*[x1d, y1d, z1d]))) #cartesian product of 1D grids
+    grid_1d_merged = [x1d, y1d, z1d]
+    print("Shape of merged 1d grid: " + str(np.shape(grid_1d_merged)))
+    print("Merged 1d grid: ")
+    print(grid_1d_merged)
+    xyz  = np.array(list(itertools.product(*grid_1d_merged))) #cartesian product of 1D grids
     return xyz 
 
 def eval_func_meshgrid_2D(x2d,y2d,func):
@@ -66,10 +70,43 @@ def tanh2d(x,y):
 
 if __name__ == "__main__":
     """ TESTS of gridtools"""
-    yrange = zrange = xrange = (0,5)
-    nptsx = nptsy = nptsz = 2
+    yrange = zrange = xrange = (1,3)
+    nptsx = nptsy = nptsz = 3
     xyz_mesh = gen_meshgrid_3D(xrange,yrange,zrange,nptsx,nptsy,nptsz)
     print(xyz_mesh[0].shape)
+
+    """ BASE TEST OF VECTOR SHAPES AND TRANSFORMATIONS"""
+    X_seq = xyz_mesh[0].reshape(-1) #sequence of numbers
+    X_seqT = X_seq.T
+   
+    X_row = xyz_mesh[0].reshape(1,-1) #row vector
+    X_rowT = X_row.T  
+
+    X_column = xyz_mesh[0].reshape((-1,1)) #column vector
+    X_columnT = X_column.T
+
+    print("Shape of X_seq:" + str(X_seq.shape))
+    print("Shape of X_seqT:" + str(X_seqT.shape))
+
+    print("Shape of X_row:" + str(X_row.shape))
+    print("Shape of X_rowT:" + str(X_rowT.shape))
+
+    print("Shape of X_column:" + str(X_column.shape))
+    print("Shape of X_columnT:" + str(X_columnT.shape))
+
+    print("Ordering after reshape for xyz_mesh is: " + str(X_row))
+    print("Ordering after reshape for xyz_mesh is: " + str(X_column))
+
+    s1  = np.einsum('ij,ji->',X_row,X_column) #returns a scalar
+    s1a = np.einsum('ij,ji->i',X_row,X_column) #returns 1-element sequence
+    s2  = np.dot(X_row,X_column) #returns array of size (1,1)
+
+    print("Shape of np.einsum result is: " + str(s1.shape))
+    print("Shape of np.einsum2 result is: " + str(s1a.shape))
+    print("Shape of np.dot result is: " + str(s2.shape))
+    print(s1,s2[0,0])
+
+ 
 
     xyz_flat = gen_cartgrid_3D(xrange,yrange,zrange,nptsx,nptsy,nptsz)    
     print(xyz_flat.shape)
